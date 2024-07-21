d76e0c477a6af0380f77b8e2f14d65d5968d1444
DilanJT <dicmandilan@gmail.com>
2024-07-21 12:36:51 +0530

    variables basics

---
+++ datatypes/variables.py
@@ -0,0 +1,32 @@
+
+# variable declaration and initialization
+
+first_value = 4 # declaration and initialization happens at the same time
+
+FirstValue = 4
+
+
+First = 4
+first = 3
+first3 = 9
+fi3rst = 8
+
+print("first :", first)
+print("First :", First)
+
+defyet = 7
+
+
+x = "kavya "
+y = "is "
+z = 21
+w = "years old."
+print(x + y + str(z) + " " + w)
+
+
+name = None
+name = "dilan"
+print(type(name))
+name = 32
+print(type(name))
+print(name)
