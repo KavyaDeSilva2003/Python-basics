dac240c15a40b7714179ad0569c3a96f5da4b30e
KavyaDeSilva2003 <161176091+KavyaDeSilva2003@users.noreply.github.com>
2024-07-30 23:51:39 +0800

    flow chart commited

---
+++ datatypes/Practice questions.py
@@ -0,0 +1,55 @@
+# Integer
+integer_var = 10
+print("Integer:", integer_var, "Type:", type(integer_var))
+
+# Float
+float_var = 10.5
+print("Float:", float_var, "Type:", type(float_var))
+
+# String
+string_var = "Hello, Python!"
+print("String:", string_var, "Type:", type(string_var))
+
+# Boolean
+boolean_var = True
+print("Boolean:", boolean_var, "Type:", type(boolean_var))
+
+# List
+list_var=[1,2,3,4]
+print("list_var:", list_var, "Type:",  type(list_var))
+
+#tuple
+tuple_var={1,2,3,4}
+print("tuple_var:", tuple_var, "Type:", type(tuple_var))
+
+
+#dictionary
+dict_var={"key1": "value1", "key2": "value2"}
+print("dict_var:", dict_var, "Type:", type(dict_var))
+
+#set
+set_var={1,2,3,4}
+print("set_var:", set_var, "Type:", type(set_var))
+
+# Variables
+x = 20
+y = 15
+
+# Equal
+print("Equal:", x == y)
+
+# Not Equal
+print("Not Equal:", x != y)
+
+# Greater Than
+print("Greater Than:", x > y)
+
+# Less Than
+print("Less Than:", x < y)
+
+# Greater Than or Equal
+print("Greater Than or Equal:", x >= y)
+
+# Less Than or Equal
+print("Less Than or Equal:", x <= y)
+
---
+++ datatypes/flow_chart.py
@@ -0,0 +1,10 @@
+num_one = int(input("Enter number one:"))
+num_two = int(input("Enter number two:"))
+
+answer = num_one / num_two
+print("type", type(answer))
+print("answer:", answer)
+
+# username = input("Enter username:")
+# print("Type", type(username))
+# print("Username is: " + username)
